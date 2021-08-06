fb50038081698436ba15f048d80604231a86abb8
ankitsrao <ankitrao7739@gmail.com>
2021-08-06 16:22:14 +0530

    Updated the notes section of Windows baseline scripts

--- WindowsServer2012R2/CSBP_WindowsServer2012_R2.ps1
+++ WindowsServer2012R2/CSBP_WindowsServer2012_R2.ps1
@@ -2,9 +2,9 @@
 .SYNOPSIS
     DSC script to harden Windows Server 2012 R2 VM baseline policies for CSBP.
 .DESCRIPTION
-    The script to harden Windows Server 2012 R2 VM baseline policies for CSBP using Desired State Configurations (DSC) for CIS Benchmark Windows Server 2016 Version 1.0.0 supported by Cloudneeti.
+     This script aims to harden Windows Server 2012 R2 VM baseline policies using Desired State Configurations (DSC) for CIS Benchmark Windows Server 2012 R2 Version 1.0.0 supported by ZCSPM.
 .NOTE
-    Copyright (c) Cloudneeti. All rights reserved.
+    Copyright (c) ZCSPM. All rights reserved.
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
